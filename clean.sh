e32d368e5055a72806dd903d05aa2259e0e948c9
Rick Mesta <rick.mesta@1791.com>
2024-07-31 17:53:04 -0500

    AELD: Solution to Assignment 4 (Part 2)
    Signed-off-by: Rick Mesta <rick.mesta@1791.com>

---
+++ clean.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Rick Mesta
+# 07/31/2024
+#
+# University of Colorado at Boulder
+# ECEN 5713: Advanced Embedded Linux Development
+# Assignment 4 (Part 2)
+#
+# Convenience script to clean buildroot distro
+#
+make -C buildroot distclean
